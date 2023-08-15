e6a2980b5bac97c5341d084d66a40ed9af8f3e91
Matthew Theiley <matthewtheiley@Matthews-MacBook-Pro-2.local>
2023-08-15 15:17:31 +0930

    adding basic styling for clone first

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {
+      bprimary: '#313338',
+      bsecondary: '#282b30',
+      btrim: '#1e2124',
+      baccent: '#7289da',
+      baccent2: '#383a40',
+      tprimary: '#ffffff',
+      tsecondary: '#8c929b',
+      taccent: '#8c8f93',
+      red: '#EB459E',
+      green: '#57F287',
+      yellow: '#FEE75C'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
+
